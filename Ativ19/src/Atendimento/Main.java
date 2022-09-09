67bfe10aa3660820fa1b2ed79ec35f61951842aa
Davi Lima da Silva <95945759+DavLM@users.noreply.github.com>
2022-09-09 17:44:53 -0300

    Update Main.java

--- Ativ19/src/Atendimento/Main.java
+++ Ativ19/src/Atendimento/Main.java
@@ -1,6 +1,8 @@
 /*Exercício 19 - Identifique as classes e implemente um programa para a seguinte especificação: "O supermercado vende diferentes 
 tipos de produtos. Cada produto tem um preço e uma quantidade em estoque.Um pedido de um cliente é composto de itens, onde cada 
-item especifica o produto que o cliente deseja e a respectica quantidade.Esse pedido pode ser pago em dinheiro, cheque ou cartão*/
+item especifica o produto que o cliente deseja e a respectica quantidade.Esse pedido pode ser pago em dinheiro, cheque ou cartão
+OBS: Infelizmente o laço de repetição não termina, fazendo com que o mesmo fique "infinitamente" e a lista está quebrada, não consegui
+desenvolver o mesmo ao ponto de criar a lista de forma efetiva, espero que possa me ajudar a encontrar meus erros.*/
 
 package Atendimento;
 
@@ -120,5 +122,3 @@ public class Main {
 		
     }
     }
-
-21321321321321
